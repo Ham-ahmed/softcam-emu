a640026121b3e6badb85589dffda9634beb14de2
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-03-16 00:16:47 +0200

    Add files via upload

---
+++ softcams-supcam-oscam_11748-r799_all.sh
@@ -0,0 +1,12 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/enigma2-plugin-softcams-supcam-oscam_11748-emu-r799_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/softcam-emu/main/enigma2-plugin-softcams-supcam-oscam_11748-emu-r799_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-softcams-supcam-oscam_11748-emu-r799_all.ipk
+wait
+sleep 2;
+exit 0
