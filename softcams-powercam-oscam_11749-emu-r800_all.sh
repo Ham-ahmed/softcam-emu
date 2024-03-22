bc472993287949265e7b785d285a6048aaeabdfb
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-03-22 22:06:24 +0200

    Add files via upload

---
+++ softcams-powercam-oscam_11749-emu-r800_all.sh
@@ -0,0 +1,12 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/enigma2-plugin-softcams-powercam-oscam_11749-emu-r800_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/softcam-emu/main/enigma2-plugin-softcams-powercam-oscam_11749-emu-r800_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-softcams-powercam-oscam_11749-emu-r800_all.ipk
+wait
+sleep 2;
+exit 0
