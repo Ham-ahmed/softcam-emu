efbdb89510be7ad4921e0579a3fa31ab684e227d
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-04-01 22:45:20 +0200

    Add files via upload

--- softcams-supcam-ncam_14.7-r0_all.sh
+++ softcams-supcam-ncam_14.7-r0_all.sh
@@ -2,11 +2,11 @@
 #!/bin/sh
 #
 
-wget -O /var/volatile/tmp/enigma2-plugin-softcams-supcam-ncam_14.7-r0_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/softcam-emu/main/enigma2-plugin-softcams-supcam-ncam_14.7-r0_all.ipk"
+wget -O /var/volatile/tmp/enigma2-plugin-softcams-supcam-ncam_14.9-r0_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/softcam-emu/main/enigma2-plugin-softcams-supcam-ncam_14.9-r0_all.ipk"
 wait
 opkg install --force-overwrite /tmp/*.ipk
 wait
-rm -f /var/volatile/tmp/enigma2-plugin-softcams-supcam-ncam_14.7-r0_all.ipk
+rm -f /var/volatile/tmp/enigma2-plugin-softcams-supcam-ncam_14.9-r0_all.ipk
 wait
 sleep 2;
 exit 0
