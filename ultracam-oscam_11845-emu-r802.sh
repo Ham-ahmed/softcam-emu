e71c3a845bfb6024f8ae0bc29075d1f9676d0b11
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-10-12 16:50:07 +0300

    Add files via upload

---
+++ ultracam-oscam_11845-emu-r802.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/enigma2-plugin-softcams-ultracam-oscam_11845-emu-r802_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/softcam-emu/refs/heads/main/enigma2-plugin-softcams-ultracam-oscam_11845-emu-r802_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-softcams-ultracam-oscam_11845-emu-r802_all.ipk
+wait
+sleep 2;
+echo "" 
+echo "" 
+echo "*********************************************************"
+echo "#                   INSTALLED SUCCESSFULLY              #"
+echo "*                       ON - Panel                      *"
+echo "*                Enigma2 restart is required            *"
+echo "*********************************************************"
+echo "               UPLOADED BY  >>>>   HAMDY_AHMED           "
+sleep 4;
+	echo '================================================='
+###########################################                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "*********************************************************"
+wait
+killall -9 enigma2
+exit 0
